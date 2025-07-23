4d90896be64e5c5adbfd36151eac7acb298650c2
Murat Shagirov <muratshagirov@gmail.com>
2025-07-24 00:53:23 +0800

    dprint for markdown

--- init.lua
+++ init.lua
@@ -679,6 +679,7 @@ require('lazy').setup({
         clangd = {},
         gopls = {},
         pyright = {},
+        dprint = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
